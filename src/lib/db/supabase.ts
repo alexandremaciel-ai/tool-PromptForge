285729c11a6a78cc3652e0fa84ce5bedc5e13f59
Alexandre Maciel <alemaomaciel@icloud.com>
2026-03-29 09:39:08 -0300

    chore: remove hardcoded supabase key and update gitignore to exclude environment-specific local files

--- src/lib/db/supabase.ts
+++ src/lib/db/supabase.ts
@@ -2,7 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 
 // Pegando do .env_local (Supabase Local) ou Produção
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "http://127.0.0.1:54321";
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...";
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
